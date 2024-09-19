e424d359db5ac4e4b3a7b309d3900df5b27b106f
Dương Trí Dũng <crytv778@gmail.com>
2024-09-19 21:05:24 +0700

    update code

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,8 +1,23 @@
+import { deepOrange } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   colorSchemes: {
     dark: true,
+    light: {
+      palette: {
+        primary: {
+          main: "#5352ed", // Đổi màu chính của giao diện tại đây
+        },
+        secondary: deepOrange,
+      },
+    },
+    // dark: {
+    //   palette: {
+    //     primary: cyan,
+    //     secondary: orange,
+    //   },
+    // },
   },
 });
 
