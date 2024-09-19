81c72ffb32cd9d5bab6e14014451a7cd77294c18
Dương Trí Dũng <crytv778@gmail.com>
2024-09-19 09:21:20 +0700

    completed

--- server/src/main/java/com/rrms/rrms/RrmsApplication.java
+++ server/src/main/java/com/rrms/rrms/RrmsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RrmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RrmsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RrmsApplication.class, args);
+    }
 }
