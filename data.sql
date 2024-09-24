c1edc5dcdc264f4bdf208bf96b64d56fc6832f84
Dương Trí Dũng <crytv778@gmail.com>
2024-09-24 18:00:49 +0700

    update environment

--- data.sql
+++ data.sql
@@ -1,102 +1,104 @@
-CREATE TABLE Roles (  
-    roleId BINARY(16) PRIMARY KEY,  
-    roleName VARCHAR(255),  
-    description TEXT 
-);  
+use rrms;
 
-CREATE TABLE Rules (  
-    ruleId BINARY(16) PRIMARY KEY,  
-    ruleName NVARCHAR(255),  
-    price DECIMAL(10, 2)  
-);  
+CREATE TABLE Roles (
+    roleId BINARY(16) PRIMARY KEY,
+    roleName VARCHAR(255),
+    description TEXT
+);
 
-CREATE TABLE Searchs (  
-    searchId BINARY(16) PRIMARY KEY,  
-    userName VARCHAR(100) not null,   
-    content TEXT  
-);  
+CREATE TABLE Rules (
+    ruleId BINARY(16) PRIMARY KEY,
+    ruleName NVARCHAR (255),
+    price DECIMAL(10, 2)
+);
 
-CREATE TABLE Motels (  
-    motelId BINARY(16) PRIMARY KEY,  
-    motelName VARCHAR(255),  
-    area DOUBLE,  
-    averagePrice DECIMAL(10, 2),  
-    address NVARCHAR(255)
-);  
+CREATE TABLE Searchs (
+    searchId BINARY(16) PRIMARY KEY,
+    userName VARCHAR(100) not null,
+    content TEXT
+);
 
-CREATE TABLE Rooms (  
-    RoomId INT NOT NULL,  
-    MobileId INT,  
-    InvoiceId INT,  
-    RoomServiceId INT,  
-    RoomDeviceId INT  
-);  
+CREATE TABLE Motels (
+    motelId BINARY(16) PRIMARY KEY,
+    motelName VARCHAR(255),
+    area DOUBLE,
+    averagePrice DECIMAL(10, 2),
+    address NVARCHAR (255)
+);
 
-CREATE TABLE Supports (  
-    SupportId INT NOT NULL,  
-    Username VARCHAR(255),  
-    DateOfStay DATE,  
-    CreateDate DATE,  
-    Price DECIMAL(10, 2)  
-);  
+CREATE TABLE Rooms (
+    RoomId INT NOT NULL,
+    MobileId INT,
+    InvoiceId INT,
+    RoomServiceId INT,
+    RoomDeviceId INT
+);
 
-CREATE TABLE Notifications (  
-    NotificationId INT NOT NULL,  
-    RoomId INT,  
-    UsernameLandlord VARCHAR(255),  
-    UsernameTenant VARCHAR(255),  
-    Title VARCHAR(255),  
-    Content TEXT,  
-    NumberOfRecipients INT  
-);  
+CREATE TABLE Supports (
+    SupportId INT NOT NULL,
+    Username VARCHAR(255),
+    DateOfStay DATE,
+    CreateDate DATE,
+    Price DECIMAL(10, 2)
+);
 
-CREATE TABLE NotificationRoom (  
-    NotificationRoomId INT NOT NULL,  
-    RoomId INT,  
-    NotificationId INT  
-);  
+CREATE TABLE Notifications (
+    NotificationId INT NOT NULL,
+    RoomId INT,
+    UsernameLandlord VARCHAR(255),
+    UsernameTenant VARCHAR(255),
+    Title VARCHAR(255),
+    Content TEXT,
+    NumberOfRecipients INT
+);
 
-CREATE TABLE Accounts (  
-    Username VARCHAR(255) NOT NULL,  
-    Password VARCHAR(255),  
-    Fullname VARCHAR(255),  
-    Phone VARCHAR(20),  
-    Email VARCHAR(255),  
-    Birthday DATE,  
-    Gender VARCHAR(10),  
-    OID INT  
-);  
+CREATE TABLE NotificationRoom (
+    NotificationRoomId INT NOT NULL,
+    RoomId INT,
+    NotificationId INT
+);
 
-CREATE TABLE BulletinBoards (  
-    BulletinBoardId INT NOT NULL,  
-    RoomId INT,  
-    Username VARCHAR(255),  
-    Title VARCHAR(255),  
-    DateOfCity DATE  
-);  
+CREATE TABLE Accounts (
+    Username VARCHAR(255) NOT NULL,
+    Password VARCHAR(255),
+    Fullname VARCHAR(255),
+    Phone VARCHAR(20),
+    Email VARCHAR(255),
+    Birthday DATE,
+    Gender VARCHAR(10),
+    OID INT
+);
 
-CREATE TABLE Devices (  
-    DeviceId INT NOT NULL,  
-    DeviceName VARCHAR(255),  
-    Available BOOLEAN  
-);  
+CREATE TABLE BulletinBoards (
+    BulletinBoardId INT NOT NULL,
+    RoomId INT,
+    Username VARCHAR(255),
+    Title VARCHAR(255),
+    DateOfCity DATE
+);
 
-CREATE TABLE RoomDevices (  
-    RoomDeviceId INT NOT NULL,  
-    RoomId INT,  
-    DeviceId INT,  
-    Quantity INT  
-);  
+CREATE TABLE Devices (
+    DeviceId INT NOT NULL,
+    DeviceName VARCHAR(255),
+    Available BOOLEAN
+);
 
-CREATE TABLE RoomServices (  
-    RoomServiceId INT NOT NULL,  
-    RoomId INT,  
-    ServiceId INT  
-);  
+CREATE TABLE RoomDevices (
+    RoomDeviceId INT NOT NULL,
+    RoomId INT,
+    DeviceId INT,
+    Quantity INT
+);
 
-CREATE TABLE Services (  
-    ServiceId INT NOT NULL,  
-    TypeService VARCHAR(255),  
-    NameService VARCHAR(255),  
-    Price DECIMAL(10, 2)  
+CREATE TABLE RoomServices (
+    RoomServiceId INT NOT NULL,
+    RoomId INT,
+    ServiceId INT
+);
+
+CREATE TABLE Services (
+    ServiceId INT NOT NULL,
+    TypeService VARCHAR(255),
+    NameService VARCHAR(255),
+    Price DECIMAL(10, 2)
 );
