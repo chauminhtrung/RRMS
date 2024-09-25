305d629f08c4f2bdf8e19a6120396f0721c31c03
Dương Trí Dũng <crytv778@gmail.com>
2024-09-25 18:18:32 +0700

    change lib carousel

---
+++ client/src/pages/Detail/NextArrow.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
+
+const NextArrow = ({ onClick, visible }) => {
+  return (
+    <div
+      onClick={onClick}
+      style={{
+        position: "absolute",
+        top: "50%",
+        right: 10,
+        zIndex: 1,
+        cursor: "pointer",
+        opacity: visible ? 1 : 0,
+        transform: visible ? "translateX(0)" : "translateX(20px)",
+        transition: "opacity 0.5s ease, transform 0.5s ease",
+      }}
+    >
+      <ArrowCircleRightIcon sx={{ color: "#f1f2f6" }} fontSize="large" />
+    </div>
+  );
+};
+
+export default NextArrow;
