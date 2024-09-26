34787ab90b26409586bc0b36ae55f25941c1c170
Dương Trí Dũng <crytv778@gmail.com>
2024-09-26 21:54:10 +0700

    tiếp tục

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -28,14 +28,8 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'on',
+      'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'no-restricted-imports': [
-        'error',
-        {
-          patterns: ['@mui/*/*/*'],
-        },
-      ],
     },
   },
 ]
