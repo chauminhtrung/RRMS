8e2ec8175d540ecad1469c15b74287c68662f4e7
Dương Trí Dũng <crytv778@gmail.com>
2024-09-26 10:13:37 +0700

    add PWA to project

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Room Rental Management System',
+        short_name: 'RRMS',
+        description: 'My Progressive Web App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/assets/house.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/assets/house.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: [{ find: '~', replacement: '/src' }],
   },
