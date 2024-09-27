a37e739eff8adda66c243f33fb40a19d39889d08
Dương Trí Dũng <crytv778@gmail.com>
2024-09-27 15:17:12 +0700

    format code

--- client/src/pages/Homes/FeatureCard.jsx
+++ client/src/pages/Homes/FeatureCard.jsx
@@ -1,18 +1,20 @@
-import PropTypes from 'prop-types';  
-import { Typography } from '@mui/material';  
+import PropTypes from 'prop-types'
+import { Typography } from '@mui/material'
 
-const FeatureCard = ({ title, description }) => {  
-    return (  
-        <div className="feature-card">  
-            <Typography variant="h6" component="h3">{title}</Typography>  
-            <Typography variant="body2">{description}</Typography>  
-        </div>  
-    );  
-};  
+const FeatureCard = ({ title, description }) => {
+  return (
+    <div className="feature-card">
+      <Typography variant="h6" component="h3">
+        {title}
+      </Typography>
+      <Typography variant="body2">{description}</Typography>
+    </div>
+  )
+}
 
-FeatureCard.propTypes = {  
-    title: PropTypes.string.isRequired,  
-    description: PropTypes.string.isRequired,  
-};  
+FeatureCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
 
-export default FeatureCard;
+export default FeatureCard
