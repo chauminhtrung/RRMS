0ca87626ede9672b2b35447a1498c4ae62c0da88
trivu2004 <kahuka2001@gmail.com>
2024-09-27 14:06:11 +0700

    update page search hover img and formatterAmount

--- client/src/apis/mock-data-search.js
+++ client/src/apis/mock-data-search.js
@@ -1,57 +1,56 @@
 export const search = {
-  name: "Chung cư Mini Trần Cung 3,3tr tháng",
-  address: "Bắc Từ Liêm",
-  addressDetail:
-    "53 ngõ 514 Trần Cung, Cổ Nhuế 1, Bắc Từ Liêm, Thành phố Hà Nội",
-  type: "Chung cư mini",
-  rentalStartTime: "01/03/2024",
+  name: 'Chung cư Mini Trần Cung 3,3tr tháng',
+  address: 'Bắc Từ Liêm',
+  addressDetail: '53 ngõ 514 Trần Cung, Cổ Nhuế 1, Bắc Từ Liêm, Thành phố Hà Nội',
+  type: 'Chung cư mini',
+  rentalStartTime: '01/03/2024',
   price: 3300000,
-  acreage: 18,
-  electricity: 3.8,
-  water: 25.0,
-  avata: "https://via.placeholder.com/150",
+  acreage: 40,
+  electricity: 38000,
+  water: 25000,
+  avata: 'https://via.placeholder.com/150',
   alt: [
     {
-      nameAlt: "Trinh",
+      nameAlt: 'Trinh',
     },
     {
-      nameAlt: "Tú",
+      nameAlt: 'Tú',
     },
     {
-      nameAlt: "An",
+      nameAlt: 'An',
     },
     {
-      nameAlt: "Thuận",
+      nameAlt: 'Thuận',
     },
     {
-      nameAlt: "Tiến",
+      nameAlt: 'Tiến',
     },
     {
-      nameAlt: "Dũng",
+      nameAlt: 'Dũng',
     },
   ],
-  online: "Trinh, 2 ngày trước",
+  online: 'Trinh, 2 ngày trước',
   images: [
     {
-      image: "https://picsum.photos/1000/500?random=1",
+      image: 'https://picsum.photos/1000/500?random=1',
     },
     {
-      image: "https://picsum.photos/1000/500?random=2",
+      image: 'https://picsum.photos/1000/500?random=2',
     },
     {
-      image: "https://picsum.photos/1000/500?random=3",
+      image: 'https://picsum.photos/1000/500?random=3',
     },
     {
-      image: "https://picsum.photos/1000/500?random=4",
+      image: 'https://picsum.photos/1000/500?random=4',
     },
     {
-      image: "https://picsum.photos/1000/500?random=5",
+      image: 'https://picsum.photos/1000/500?random=5',
     },
     {
-      image: "https://picsum.photos/1000/500?random=6",
+      image: 'https://picsum.photos/1000/500?random=6',
     },
     {
-      image: "https://picsum.photos/1000/500?random=7",
+      image: 'https://picsum.photos/1000/500?random=7',
     },
   ],
-};
+}
