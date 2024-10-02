071244935ba1ed5b3b4dcddfb973d969d2929891
Dương Trí Dũng <crytv778@gmail.com>
2024-10-02 19:09:13 +0700

    chore: delete ImageController.java, Image.java and ImageRepository.java

--- server/src/main/java/com/rrms/rrms/repositories/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rrms.rrms.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rrms.rrms.models.Image;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {}
