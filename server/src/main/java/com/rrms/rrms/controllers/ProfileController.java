274819bcff77df8287d1f91f81e52ffa42c00028
Dương Trí Dũng <crytv778@gmail.com>
2024-10-02 09:25:20 +0700

    add Annotation into ProfileController

--- server/src/main/java/com/rrms/rrms/controllers/ProfileController.java
+++ server/src/main/java/com/rrms/rrms/controllers/ProfileController.java
@@ -1,2 +1,10 @@
-package com.rrms.rrms.controllers;public class ProfileController {
-}
+package com.rrms.rrms.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@CrossOrigin("*")
+public class ProfileController {}
