e94c1b0f91ddd23defa4779a39ab7d12a3a837bf
Dương Trí Dũng <crytv778@gmail.com>
2024-10-02 19:09:29 +0700

    update data.sql and ErrorCode.java

--- server/src/main/java/com/rrms/rrms/enums/ErrorCode.java
+++ server/src/main/java/com/rrms/rrms/enums/ErrorCode.java
@@ -6,9 +6,9 @@ import org.springframework.http.HttpStatusCode;
 
 @Getter
 public enum ErrorCode {
-    ACCOUNT_NOT_FOUND(1003, "Account not found", HttpStatus.NOT_FOUND),
+    ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Account not found", HttpStatus.NOT_FOUND),
 
-    SEARCH_NOT_FOUND(1003, "Search not found", HttpStatus.NOT_FOUND);
+    SEARCH_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Search not found", HttpStatus.NOT_FOUND);
 
     ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
