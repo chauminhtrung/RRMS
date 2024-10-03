64818a7566155605466648ca8e086b18543b1a22
trivu2004 <kahuka2001@gmail.com>
2024-10-03 13:36:06 +0700

    confix

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,13 +1,8 @@
-
-import React from 'react'
-
 import FacebookIcon from '@mui/icons-material/Facebook'
 import GoogleIcon from '@mui/icons-material/Google'
 import TwitterIcon from '@mui/icons-material/Twitter'
 import InstagramIcon from '@mui/icons-material/Instagram'
 import './Login.css'
-
-import { Link } from 'react-router-dom'
 
 import { Link } from 'react-router-dom'
 
@@ -52,9 +47,6 @@ const Login = () => {
                 <h3>Đăng nhập tài khoản</h3>
 
                 <form method="POST" className="needs-validation" id="login-form">
-
-                <form method="POST" className="needs-validation" id="login-form" novalidate="">
-
                   <input type="hidden" name="_token" value="jkJEHM7McmP1rRwYRqXYkCRrsj0oZZEtjCSD9vlp" />{' '}
                   <div className="container-input">
                     <div className="form-group">
@@ -147,56 +139,52 @@ const Login = () => {
                   <div className="row mt-2 g-2">
                     <div
                       className="col-sm-12 col-md-6 text-center text-md-end aos-init aos-animate"
-
                       data-aos="fade-right"></div>
                     <div
                       className="col-sm-12 col-md-6 text-center text-md-start aos-init aos-animate"
                       data-aos="fade-left"></div>
 
-                      data-aos="fade-right">
-                      <a href="https://play.google.com/store/apps/details?id=com.lozido_room_mobile" target="_blank">
-                        <img
-                          className="img-responsive image-dowload-app"
-                          width="160px"
-                          src="https://quanlytro.me/images/tai-ung-dung-tim-phong-tro-tren-android.png"
-                          alt="Tải ứng dụng trên Android"
-                          title="Tải ứng dụng trên Android"
-                        />
-                      </a>
-                    </div>
-                    <div
-                      className="col-sm-12 col-md-6 text-center text-md-start aos-init aos-animate"
-                      data-aos="fade-left">
-                      <a href="https://apps.apple.com/vn/app/id1560928216" target="_blank">
-                        <img
-                          className="img-responsive image-dowload-app"
-                          width="160px"
-                          src="https://quanlytro.me/images/tai-ung-dung-tim-phong-tro-tren-ios.png"
-                          alt="Tải ứng dụng trên iOS"
-                          title="Tải ứng dụng trên iOS"
-                        />
-                      </a>
-                    </div>
-                    <p style={{ marginTop: '15px', fontSize: ' 13px' }} className="text-center">
-                      * Để đăng nhập bạn phải có tài khoản. Vui lòng đăng ký tài khoản nếu chưa có
-                    </p>
-
+                    <a href="https://play.google.com/store/apps/details?id=com.lozido_room_mobile" target="_blank">
+                      <img
+                        className="img-responsive image-dowload-app"
+                        width="160px"
+                        src="https://quanlytro.me/images/tai-ung-dung-tim-phong-tro-tren-android.png"
+                        alt="Tải ứng dụng trên Android"
+                        title="Tải ứng dụng trên Android"
+                      />
+                    </a>
                   </div>
                   <div
                     className="col-sm-12 col-md-6 text-center text-md-start aos-init aos-animate"
-                    data-aos="fade-left"></div>
-
+                    data-aos="fade-left">
+                    <a href="https://apps.apple.com/vn/app/id1560928216" target="_blank">
+                      <img
+                        className="img-responsive image-dowload-app"
+                        width="160px"
+                        src="https://quanlytro.me/images/tai-ung-dung-tim-phong-tro-tren-ios.png"
+                        alt="Tải ứng dụng trên iOS"
+                        title="Tải ứng dụng trên iOS"
+                      />
+                    </a>
+                  </div>
                   <p style={{ marginTop: '15px', fontSize: ' 13px' }} className="text-center">
                     * Để đăng nhập bạn phải có tài khoản. Vui lòng đăng ký tài khoản nếu chưa có
                   </p>
                 </div>
+                <div
+                  className="col-sm-12 col-md-6 text-center text-md-start aos-init aos-animate"
+                  data-aos="fade-left"></div>
+
+                <p style={{ marginTop: '15px', fontSize: ' 13px' }} className="text-center">
+                  * Để đăng nhập bạn phải có tài khoản. Vui lòng đăng ký tài khoản nếu chưa có
+                </p>
               </div>
             </div>
-            <div className="text-center mt-5">
-              <p>
-                Copyright @ <strong>RRMS - Quản lý nhà cho thuê</strong>
-              </p>
-            </div>
+          </div>
+          <div className="text-center mt-5">
+            <p>
+              Copyright @ <strong>RRMS - Quản lý nhà cho thuê</strong>
+            </p>
           </div>
         </div>
       </div>
