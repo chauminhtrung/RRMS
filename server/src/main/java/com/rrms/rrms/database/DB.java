bd9ab908d4eb14d80beff75721353e62efa836a6
Dương Trí Dũng <crytv778@gmail.com>
2024-10-04 08:44:49 +0700

    add cache by redis to profile

--- server/src/main/java/com/rrms/rrms/database/DB.java
+++ server/src/main/java/com/rrms/rrms/database/DB.java
@@ -1,8 +1,11 @@
 package com.rrms.rrms.database;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.rrms.rrms.enums.Gender;
 import com.rrms.rrms.models.Account;
 import com.rrms.rrms.models.Motel;
@@ -12,24 +15,18 @@ import com.rrms.rrms.repositories.AccountRepository;
 import com.rrms.rrms.repositories.MotelRepository;
 import com.rrms.rrms.repositories.RoomRepository;
 import com.rrms.rrms.repositories.TypeRoomRepository;
-import com.rrms.rrms.services.AccountService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Locale;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @Slf4j
 public class DB {
     @Bean
-    CommandLineRunner initDatabase(AccountRepository accountRepository,
-                                   RoomRepository roomRepository,
-                                   MotelRepository motelRepository,
-                                   TypeRoomRepository typeRoomRepository) {
+    CommandLineRunner initDatabase(
+            AccountRepository accountRepository,
+            RoomRepository roomRepository,
+            MotelRepository motelRepository,
+            TypeRoomRepository typeRoomRepository) {
         return args -> {
             if (accountRepository.findByUsername("admin").isEmpty()) {
                 // create admin account
@@ -41,10 +38,27 @@ public class DB {
                         .phone("0333333333")
                         .cccd("admin")
                         .gender(Gender.MALE)
-                        .avatar("https://firebasestorage.googleapis.com/v0/b/rrms-b7c18.appspot.com/o/images%2Faccount-avatar%2F1493af7e-ba1f-48d8-b2c8-f4e88b55e07f?alt=media&token=9e82b5f9-3f49-4856-b009-bfd09fa474c9")
+                        .avatar(
+                                "https://firebasestorage.googleapis.com/v0/b/rrms-b7c18.appspot.com/o/images%2Faccount-avatar%2F1493af7e-ba1f-48d8-b2c8-f4e88b55e07f?alt=media&token=9e82b5f9-3f49-4856-b009-bfd09fa474c9")
                         .birthday(LocalDate.now())
                         .build());
                 log.info("Admin user created");
+            }
+            if (accountRepository.findByUsername("user1").isEmpty()) {
+                // create admin account
+                accountRepository.save(Account.builder()
+                        .username("user1")
+                        .password("user1")
+                        .fullname("Tri Dung")
+                        .email("tridung@gmail.com")
+                        .phone("03333345553")
+                        .cccd("012345678900")
+                        .gender(Gender.OTHER)
+                        .avatar(
+                                "https://firebasestorage.googleapis.com/v0/b/rrms-b7c18.appspot.com/o/images%2Faccount-avatar%2F1493af7e-ba1f-48d8-b2c8-f4e88b55e07f?alt=media&token=9e82b5f9-3f49-4856-b009-bfd09fa474c9")
+                        .birthday(LocalDate.now())
+                        .build());
+                log.info("User1 created");
             }
             if (roomRepository.findAllByNameRoom("Gò Vấp").isEmpty()) {
                 // create admin account
@@ -54,18 +68,14 @@ public class DB {
                 TypeRoom typeRoom = new TypeRoom();
                 typeRoomRepository.save(typeRoom);
                 roomRepository.save(Room.builder()
-
-                                .motel(motel)
-                                .price(1111)
-
-                                .nameRoom("Hà đặc")
-                                .typeRoom(typeRoom)
-                                .description("như con cặc")
-
+                        .motel(motel)
+                        .price(1111)
+                        .nameRoom("Hà đặc")
+                        .typeRoom(typeRoom)
+                        .description("như con cặc")
                         .build());
                 log.info("Search room created");
             }
-
         };
     }
 }
