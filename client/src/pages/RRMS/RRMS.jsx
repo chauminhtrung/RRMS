0fc10d2819b06db9363600a102d4719d1c93273a
Dương Trí Dũng <crytv778@gmail.com>
2024-10-06 07:04:07 +0700

    fix deploy continute

--- client/src/pages/RRMS/RRMS.jsx
+++ client/src/pages/RRMS/RRMS.jsx
@@ -1,6 +1,6 @@
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
-import Rooms from '~/utils/Rooms'
-import Pkeyw from '~/utils/PoKey'
+import { Rooms } from '~/utils/Rooms'
+import { Pkeyw } from '~/utils/PoKey'
 import FilterSearch from '../search/FilterSearch'
 import { data } from '~/utils/slider'
 import { sliderSettings } from '~/utils/common.js'
