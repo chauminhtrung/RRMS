1e03a35037ee1825e456a57533b65a8e30969b83
Dương Trí Dũng <crytv778@gmail.com>
2024-10-15 19:11:00 +0700

    delete room by roomId

--- server/src/main/java/com/rrms/rrms/controllers/RoomController.java
+++ server/src/main/java/com/rrms/rrms/controllers/RoomController.java
@@ -1,22 +1,22 @@
 package com.rrms.rrms.controllers;
 
-import java.util.List;
-import java.util.UUID;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
 import com.rrms.rrms.dto.request.RoomRequest;
 import com.rrms.rrms.dto.response.ApiResponse;
 import com.rrms.rrms.dto.response.PostRoomTableResponse;
 import com.rrms.rrms.dto.response.RoomDetailResponse;
 import com.rrms.rrms.services.IRoom;
 import com.rrms.rrms.utils.CacheChecked;
-
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @Slf4j
@@ -28,13 +28,25 @@ public class RoomController {
     IRoom roomService;
     CacheChecked cacheChecked;
 
-    //    @Cacheable(value = "room", key = "#roomId")
+    @PostMapping
+    public ApiResponse<RoomDetailResponse> createRoom(@RequestBody RoomRequest roomRequest) {
+        RoomDetailResponse createdRoom = roomService.createRoom(roomRequest);
+        log.info("Created room at roomId: {}", createdRoom.getRoomId());
+        return ApiResponse.<RoomDetailResponse>builder()
+                .code(HttpStatus.CREATED.value())
+                .message("success")
+                .result(createdRoom)
+                .build();
+    }
+
+    @Cacheable(value = "room", key = "#roomId")
     @GetMapping("/{roomId}")
     public ApiResponse<RoomDetailResponse> getRoom(@PathVariable("roomId") UUID roomId) {
         RoomDetailResponse room = roomService.getRoomById(roomId);
         if (cacheChecked.cacheHit(room.toString(), "room")) {
             log.warn("Cache miss for Room with {}: ", roomId);
         }
+        log.info("Get room at roomId: {}", room.getRoomId());
         return ApiResponse.<RoomDetailResponse>builder()
                 .code(HttpStatus.OK.value())
                 .message("success")
@@ -45,7 +57,7 @@ public class RoomController {
     @GetMapping("/nocache/{roomId}")
     public ApiResponse<RoomDetailResponse> getRoomNoCache(@PathVariable("roomId") UUID roomId) {
         RoomDetailResponse room = roomService.getRoomById(roomId);
-        log.info("Get room: {}", room);
+        log.info("Get room no cache at roomId: {}", room.getRoomId());
         return ApiResponse.<RoomDetailResponse>builder()
                 .code(HttpStatus.OK.value())
                 .message("success")
@@ -53,23 +65,28 @@ public class RoomController {
                 .build();
     }
 
-    @PostMapping
-    public ApiResponse<RoomDetailResponse> createRoom(@RequestBody RoomRequest roomRequest) {
-        RoomDetailResponse createdRoom = roomService.createRoom(roomRequest);
-        return ApiResponse.<RoomDetailResponse>builder()
-                .code(HttpStatus.CREATED.value())
-                .message("success")
-                .result(createdRoom)
-                .build();
-    }
-
     @GetMapping("/post-room-table")
     public ApiResponse<List<PostRoomTableResponse>> getPostRoomTable(@RequestParam("username") String username) {
         List<PostRoomTableResponse> rooms = roomService.getPostRoomTable(username);
+        log.info("Get post room table at username: {}", username);
         return ApiResponse.<List<PostRoomTableResponse>>builder()
                 .code(HttpStatus.OK.value())
                 .message("success")
                 .result(rooms)
                 .build();
+    }
+
+    @CacheEvict(value = "room", key = "#roomId")
+    @DeleteMapping("/{roomId}")
+    public ApiResponse<String> deleteRoom(@PathVariable("roomId") UUID roomId) {
+        String result = "";
+        try {
+            result = roomService.deleteRoom(roomId);
+            log.info("Delete room successful at roomId: {}", roomId);
+            return ApiResponse.<String>builder().code(HttpStatus.OK.value()).message("Delete success").result(result).build();
+        } catch (Exception e) {
+            log.error("Delete room fail at roomId: {}", roomId);
+            return ApiResponse.<String>builder().code(HttpStatus.BAD_REQUEST.value()).message("Delete fail").result(result).build();
+        }
     }
 }
