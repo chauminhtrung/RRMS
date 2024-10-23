1bd3549e6993961eee780afc299df5b136245505
trivu2004 <kahuka2001@gmail.com>
2024-10-23 14:26:24 +0700

    trans English

--- client/src/i18n/i18n.js
+++ client/src/i18n/i18n.js
@@ -4,8 +4,8 @@ import { resources } from './resources'
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: 'vi',
-  fallbackLng: 'vi',
+  lng: 'en',
+  fallbackLng: 'en',
 
   interpolation: {
     escapeValue: false,
