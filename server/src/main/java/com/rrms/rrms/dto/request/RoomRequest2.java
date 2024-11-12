379f836a5b823d54de7f09b064238f5d0f0ee8fd
Dương Trí Dũng <crytv778@gmail.com>
2024-11-12 18:30:00 +0700

    create room by excel

--- server/src/main/java/com/rrms/rrms/dto/request/RoomRequest2.java
+++ server/src/main/java/com/rrms/rrms/dto/request/RoomRequest2.java
@@ -1,10 +1,10 @@
 package com.rrms.rrms.dto.request;
 
-import java.util.Date;
-import java.util.UUID;
-
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
@@ -12,19 +12,19 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class RoomRequest2 {
-    private UUID motelId;
-    private String name;
-    private String group;
-    private Double price;
-    private String prioritize;
-    private Integer area;
-    private Double deposit;
-    private Double debt;
-    private Integer countTenant;
-    private Integer invoiceDate;
-    private Integer paymentCircle;
-    private Date moveInDate;
-    private Date contractDuration;
-    private Boolean status;
-    private String finance;
+    UUID motelId;
+    String name;
+    String group;
+    Double price;
+    String prioritize;
+    Integer area;
+    Double deposit;
+    Double debt;
+    Integer countTenant;
+    Integer invoiceDate;
+    Integer paymentCircle;
+    Date moveInDate;
+    Date contractDuration;
+    Boolean status;
+    String finance;
 }
