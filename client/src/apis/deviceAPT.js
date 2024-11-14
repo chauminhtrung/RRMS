a1e26a49447d43fefe4057401490e3468f70ae05
Mr Thuan <thuanquoctr@gmail.com>
2024-11-14 16:06:25 +0700

    done motel device

--- client/src/apis/deviceAPT.js
+++ client/src/apis/deviceAPT.js
@@ -20,7 +20,7 @@ export const insertMotelDevice = async (data) => {
 }
 export const deleteMotelDevice = async (data) => {
   const token = sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user')).token : null
-  const response = await axios.delete(`${env.API_URL}/moteldevices`, data, {
+  const response = await axios.delete(`${env.API_URL}/moteldevices/${data}`, {
     headers: {
       Authorization: `Bearer ${token}`
     }
