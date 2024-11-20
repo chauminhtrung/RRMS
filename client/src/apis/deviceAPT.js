c39900dccfefed485163318529103d4814b2500f
Mr Thuan <thuanquoctr@gmail.com>
2024-11-20 23:28:09 +0700

    room device

--- client/src/apis/deviceAPT.js
+++ client/src/apis/deviceAPT.js
@@ -40,13 +40,36 @@ export const insertRoomDevice = async (data) => {
   })
   return response.data
 }
-export const deleteRoomDevice = async (data) => {
+export const deleteRoomDevice = async (roomId, motel_device_id) => {
   const token = sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user')).token : null
-  const response = await axios.delete(`${env.API_URL}/roomdevices/${data}`, {
+  const response = await axios.delete(`${env.API_URL}/roomdevices/${roomId}/${motel_device_id}`, {
     headers: {
       Authorization: `Bearer ${token}`,
       'ngrok-skip-browser-warning': '69420'
     }
   })
+  return response.data
+}
+export const getAllDeviceByRomId = async (data) => {
+  const token = sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user')).token : null
+  const response = await axios.get(`${env.API_URL}/roomdevices/${data}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'ngrok-skip-browser-warning': '69420'
+    }
+  })
+  return response.data
+}
+export const changeQuantityRoomDevice = async (data) => {
+  const token = sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user')).token : null
+  const response = await axios.post(
+    `${env.API_URL}/roomdevices/${data.roomId}/${data.motel_device_id}/${data.quantity}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'ngrok-skip-browser-warning': '69420'
+      }
+    }
+  )
   return response.data
 }
--- client/src/pages/admin/ManagerHome/HomeWData.jsx
+++ client/src/pages/admin/ManagerHome/HomeWData.jsx
@@ -18,7 +18,13 @@ import {
   updateRoom
 } from '~/apis/roomAPI'
 import { Modal } from 'bootstrap' // Import Bootstrap Modal API
-import { getAllMotelDevices, insertRoomDevice } from '~/apis/deviceAPT'
+import {
+  changeQuantityRoomDevice,
+  deleteRoomDevice,
+  getAllDeviceByRomId,
+  getAllMotelDevices,
+  insertRoomDevice
+} from '~/apis/deviceAPT'
 const HomeWData = ({ Motel }) => {
   const { motelId } = useParams()
   const [rooms, setRooms] = useState([])
@@ -98,6 +104,27 @@ const HomeWData = ({ Motel }) => {
       console.log(error)
     }
   }
+  const handleChangeQuantityRoomDevice = async (roomId, motel_device_id, quantity) => {
+    const data = {
+      roomId: roomId,
+      motel_device_id: motel_device_id,
+      quantity: quantity
+    }
+    const response = await changeQuantityRoomDevice(data)
+    if (response.result == true) {
+      Swal.fire({
+        icon: 'success',
+        title: 'Thành công',
+        text: 'change quantity successfully!'
+      })
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Thành công',
+        text: 'change quantity failed!'
+      })
+    }
+  }
 
   // Hàm lấy dữ liệu phòng từ server
   const fetchDataRooms = async (id) => {
@@ -143,7 +170,7 @@ const HomeWData = ({ Motel }) => {
       motelDevice: {
         motel_device_id: motel_device_idParam
       },
-      quantity: 5
+      quantity: 1
     }
     const response = await insertRoomDevice(data)
     if ((response.code = 200)) {
@@ -160,9 +187,21 @@ const HomeWData = ({ Motel }) => {
       })
     }
   }
-  const cancelRoomDevice = async (room, motel_device_id) => {
-    console.log(motel_device_id)
-    console.log(roomId)
+  const cancelRoomDevice = async (roomId, motel_device_id) => {
+    const response = await deleteRoomDevice(roomId, motel_device_id)
+    if (response.result == true) {
+      Swal.fire({
+        icon: 'success',
+        title: 'Thành công',
+        text: 'RoomDevice cancel successfully!'
+      })
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Thất bại',
+        text: 'RoomDevice cancel failed!'
+      })
+    }
   }
 
   // Tạo phòng
@@ -602,6 +641,22 @@ const HomeWData = ({ Motel }) => {
   const handleDetailClick = (e, rowId) => {
     window.open(`/quanlytro/${motelId ? motelId : Motel[0].motelId}/Chi-tiet-phong/${rowId}`, '_blank')
   }
+  const [deviceByRoom, setdeviceByRoom] = useState([])
+  const fetchDeviceByRoom = async (roomId) => {
+    if (!roomId) {
+      console.warn('Room ID is invalid')
+      setdeviceByRoom([])
+      return
+    }
+    const response = await getAllDeviceByRomId(roomId)
+    console.log(response.result)
+
+    if (response.result.length > 0) {
+      setdeviceByRoom(response.result)
+    } else {
+      setdeviceByRoom([])
+    }
+  }
 
   // Hàm xử lý khi người dùng chọn một mục trong menu
   const handleItemClick = (label) => {
@@ -621,18 +676,14 @@ const HomeWData = ({ Motel }) => {
       //ham o duoi dung de xac dinh dang nhan vao phong nao
       fetchDataRooms(showMenu)
       //ham thuan fetch tai san cua phong o duoi (giong trung o cai dat dich vu)
+      fetchDeviceByRoom(showMenu)
     } else if (label === 'Ghi chú') {
       setShowMenu(null) // Đóng menu
       //ham o duoi dung de xac dinh dang nhan vao phong nao
       fetchDataRooms(showMenu)
-    } 
-    else if (label === 'Lập hóa đơn') {
+    } else if (label === 'Lập hóa đơn') {
       setShowMenu(null) // Đóng menu
-
-    } 
-    
-    
-    else {
+    } else {
       alert(`Action: ${label} on room ${showMenu}`)
     }
   }
@@ -1416,8 +1467,7 @@ const HomeWData = ({ Motel }) => {
                       {...(item.label === 'Lập hóa đơn' && {
                         'data-bs-toggle': 'modal',
                         'data-bs-target': '#invoiceModal'
-                      })}
-                      >
+                      })}>
                       {item.icon && (
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -1997,15 +2047,18 @@ const HomeWData = ({ Motel }) => {
                     {device.map((item) => (
                       <div key={item.motel_device_id} className="col-12 border p-3 d-flex align-items-center mt-1">
                         <input
-                          onChange={(e) => {
+                          onChange={async (e) => {
                             if (e.target.checked) {
-                              applyRoomDevice(room, item.motel_device_id)
+                              await applyRoomDevice(room, item.motel_device_id)
                             } else {
-                              cancelRoomDevice(room, item.motel_device_id)
+                              await cancelRoomDevice(room.roomId, item.motel_device_id)
                             }
+                            const updatedDevices = await fetchDeviceByRoom(room.roomId)
+                            setdeviceByRoom(updatedDevices.result)
                           }}
                           type="checkbox"
                           className="mx-3"
+                          checked={deviceByRoom.some((it) => it.motelDevice.motel_device_id === item.motel_device_id)}
                         />
                         <div className="flex-grow-1">
                           <h6 className="mb-1">{item.deviceName}</h6>
@@ -2023,8 +2076,16 @@ const HomeWData = ({ Motel }) => {
                         <div className="d-flex align-items-center">
                           <input
                             type="number"
+                            onChange={(e) => {
+                              handleChangeQuantityRoomDevice(room.roomId, item.motel_device_id, 200)
+                            }}
                             className="form-control text-center"
-                            defaultValue={1}
+                            value={
+                              deviceByRoom.some((it) => it.motelDevice.motel_device_id === item.motel_device_id)
+                                ? deviceByRoom.find((it) => it.motelDevice.motel_device_id === item.motel_device_id)
+                                    .quantity
+                                : 1
+                            }
                             style={{ width: '100px' }}
                           />
                           <span className="mx-2">Số lượng</span>
@@ -2242,11 +2303,8 @@ const HomeWData = ({ Motel }) => {
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close">
                   {' '}
                 </button>
-
               </div>
-              <div className="modal-body">
-
-              </div>
+              <div className="modal-body"></div>
               <div className="modal-footer modal-footer--sticky">
                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
                   <svg
@@ -2289,8 +2347,6 @@ const HomeWData = ({ Motel }) => {
       ) : (
         <> </>
       )}
-
-
     </div>
   )
 }
